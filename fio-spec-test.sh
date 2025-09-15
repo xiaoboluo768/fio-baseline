bb1aa448726cdbc8aff20a172af3d875948a2333
chasen918 <chasen918@outlook.com>
2025-09-15 18:31:06 +0800

    add seq write latency test

--- fio-spec-test.sh
+++ fio-spec-test.sh
@@ -21,17 +21,18 @@ skip_check=${skip_check:-0}
 ramp_time=${ramp_time:-60}
 
 usage() {
-    echo "Usage: $0 -d \"disk1 disk2 ...\" -t runtime [-c comp_ratio] [-b \"cpus1 cpus2 ...\"] [-f]"
+    echo "Usage: $0 -d \"disk1 disk2 ...\" -t runtime [-c comp_ratio] [-b \"cpus1 cpus2 ...\"] [-f] [-o result_name]"
     echo "Options:"
     echo "  -d  Specify NVMe device names (required, space-separated, e.g. \"nvme0n1 nvme1n1\")"
     echo "  -c  Compression ratio (default: 0)"
     echo "  -t  Runtime duration in seconds (required, must be > 0)"
     echo "  -b  CPU binding ranges (space-separated, must match device count)"
     echo "  -f  Force skip device status check"
+    echo "  -o  Specify the name of output directrory (default to timestamp)"
     exit 1
 }
 
-while getopts ":d:c:t:b:f" opt; do
+while getopts ":d:c:t:b:o:f" opt; do
     case $opt in
         d)
             IFS=' ' read -ra disks <<< "$OPTARG"
@@ -48,6 +49,9 @@ while getopts ":d:c:t:b:f" opt; do
         f)
             skip_check=1
             echo -e "Warning: Device status will be skipped!"
+            ;;
+        o)
+            output_dir=$OPTARG
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -87,10 +91,14 @@ for ((i=0; i<$disks_length; i++)); do
        exit 1
     fi
 done
+wait
 
 my_dir="$( cd "$( dirname "$0"  )" && pwd  )"
-timestamp=`date +%Y%m%d_%H%M%S`
-output_dir=${my_dir}/${timestamp}
+if [ -z ${output_dir} ];then
+    timestamp=`date +%Y%m%d_%H%M%S`
+    output_dir=${my_dir}/${timestamp}
+fi
+
 if [ ! -d "${output_dir}" ]; then mkdir -p ${output_dir}; fi
 iostat_dir=${output_dir}/iostat
 result_dir=${output_dir}/result
@@ -102,8 +110,7 @@ mkdir -p ${drv_info}
 sys_info_log=${output_dir}/sys_info.log
 test_log=${output_dir}/run_test.log
 result_csv_log=${result_dir}/result.csv
-echo "Disk,IO,BS,QD,Jobs,KIOPS,BandWidth,Unit,Latency,Unit,99.99% Latency,Unit" > $result_csv_log
-collect_test_config $test_log $disks $comp_ratio $runtime $ramp_time $skip_check $cpus_allowed_list 
+collect_test_config $test_log "${disks[*]}" $comp_ratio $runtime $ramp_time $skip_check "${cpus_allowed_list[*]}" 
 collect_sys_info $sys_info_log
 
 if [ "${comp_ratio}" != "" ]; 
@@ -126,6 +133,7 @@ for ((i=0; i<$disks_length; i++)); do
         fi
     fi
 done
+wait
 
 if (( $error_flag != 0 )); then
     while true; do
@@ -166,7 +174,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --loops=$loops ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $mode
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     #Seq write 
     mode="perf";rw="write";bs=128k;job=1;qd=128;rwmixread=0;
@@ -178,7 +186,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     #Seq read 
     mode="perf";rw="read";bs=128k;job=1;qd=128;rwmixread=0;
@@ -190,10 +198,34 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
+
+    #Latency Seq write 
+    mode="latency";rw="write";bs=4k;job=1;qd=1;rwmixread=0;
+    echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd Start" >> ${test_log}
+    iostat -xmdct $disk 1 > ${iostat_dir}/${disk}_iostat_${mode}_${rw}_${bs}_${job}job_QD${qd}.log &
+    iostat_pid=$!
+    sudo fio --percentile_list=10:20:30:40:50:60:70:80:90:99:99.9:99.99:99.999:99.9999:99.99999:99.999999:99.9999999 --ioengine=libaio --direct=1 --norandommap \
+    --randrepeat=0 --log_avg_msec=1000 --group_reporting --buffer_compress_percentage=$comp_ratio --buffer_compress_chunk=4k --filename=/dev/$disk \
+    --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
+    kill $iostat_pid > /dev/null
+    echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
+
+    #Latency Seq read 
+    mode="latency";rw="read";bs=4k;job=1;qd=1;rwmixread=0;
+    echo "[`date`] [${disk}] ${mode} ${bs} ${job}job ${qd}qd Start" >> ${test_log}
+    iostat -xmdct $disk 1 > ${iostat_dir}/${disk}_iostat_${mode}_${rw}_${bs}_${job}job_QD${qd}.log &
+    iostat_pid=$!
+    sudo fio --percentile_list=10:20:30:40:50:60:70:80:90:99:99.9:99.99:99.999:99.9999:99.99999:99.999999:99.9999999 --ioengine=libaio --direct=1 --norandommap \
+    --randrepeat=0 --log_avg_msec=1000 --group_reporting --buffer_compress_percentage=$comp_ratio --buffer_compress_chunk=4k --filename=/dev/$disk \
+    --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
+    kill $iostat_pid > /dev/null
+    echo "[`date`] [${disk}] ${mode} ${bs} ${job}job ${qd}qd End" >> ${test_log}
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     #Random write precondition
-    mode="precondition";rw="randwrite";bs=4k;job=1;qd=128;rwmixread=0;loops=1;
+    mode="precondition";rw="randwrite";bs=4k;job=1;qd=128;rwmixread=0;loops=2;
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd Start" >> ${test_log}
     iostat -xmdct $disk 1 > ${iostat_dir}/${disk}_iostat_${mode}_${rw}_${bs}_${job}job_QD${qd}.log &
     iostat_pid=$!
@@ -202,7 +234,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --loops=$loops ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $mode
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
     
     #Random write
     mode="perf";rw="randwrite";bs=4k;job=4;qd=128;rwmixread=0;
@@ -214,10 +246,10 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
-    #Latency write
-    mode="perf";rw="randwrite";bs=4k;job=1;qd=1;rwmixread=0;
+    #Latency random write
+    mode="latency";rw="randwrite";bs=4k;job=1;qd=1;rwmixread=0;
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd Start" >> ${test_log}
     iostat -xmdct $disk 1 > ${iostat_dir}/${disk}_iostat_${mode}_${rw}_${bs}_${job}job_QD${qd}.log &
     iostat_pid=$!
@@ -226,7 +258,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     #Random mix
     mode="perf";rw="randrw";bs=4k;job=4;qd=128;rwmixread=70;
@@ -238,7 +270,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --rwmixread=${rwmixread} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     #Random read
     mode="perf";rw="randread";bs=4k;job=4;qd=128;rwmixread=0;
@@ -250,10 +282,10 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
-    #Latency read
-    mode="perf";rw="randread";bs=4k;job=1;qd=1;rwmixread=0;
+    #Latency random read
+    mode="latency";rw="randread";bs=4k;job=1;qd=1;rwmixread=0;
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd Start" >> ${test_log}
     iostat -xmdct $disk 1 > ${iostat_dir}/${disk}_iostat_${mode}_${rw}_${bs}_${job}job_QD${qd}.log &
     iostat_pid=$!
@@ -262,7 +294,7 @@ function Run_test(){
     --name=${mode}_${rw}_${bs}_${job}job_QD${qd} --rw=${rw} --bs=${bs} --numjobs=${job} --iodepth=${qd} --ramp_time=$ramp_time --time_based --runtime=$runtime ${cpus_allowed_set} > ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log
     kill $iostat_pid > /dev/null
     echo "[`date`] [${disk}] ${mode} ${rw} ${bs} ${job}job ${qd}qd End" >> ${test_log}
-    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk
+    collect_fio_result ${result_dir}/${disk}_${mode}_${rw}_${bs}_${job}job_${qd}qd.log $result_csv_log $disk $comp_ratio $mode
 
     collect_drv_after_info ${disk}
     echo "[`date`] [${disk}] Test End" >> ${test_log}
@@ -281,6 +313,5 @@ for ((i=0; i<$disks_length; i++)); do
     fi
 done
 
-
-
-
+wait
+echo -e "Spec test is done, see result at ${output_dir}"
